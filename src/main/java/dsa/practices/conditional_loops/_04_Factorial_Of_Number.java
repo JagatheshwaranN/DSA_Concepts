e1d2dd68adf910ddde711bbb4fe663ede4e8639b
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-28 19:00:01 +0530

    DSA - Loops Practice Programs

--- src/main/java/dsa/practices/conditional_loops/_04_Factorial_Of_Number.java
+++ src/main/java/dsa/practices/conditional_loops/_04_Factorial_Of_Number.java
@@ -7,14 +7,18 @@ public class _04_Factorial_Of_Number {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to find the factorial");
         int input = scanner.nextInt();
-        System.out.println(findFactorial(input));
+        System.out.println("The factorial of "+input+ " is "+findFactorial(input));
     }
 
     private static int findFactorial(int num){
 
-        int factorial = 0;
-
+        int factorial = 1;
+        while(num > 0){
+            factorial = factorial * num;
+            num = num -1;
+        }
         return factorial;
     }
 }
