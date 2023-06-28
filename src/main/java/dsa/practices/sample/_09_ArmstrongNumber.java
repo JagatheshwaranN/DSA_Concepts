a97e2a8845ab9703e93cbc8869f51ac82ab11ce4
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-28 15:22:07 +0530

    DSA - Practice Programs

---
+++ src/main/java/dsa/practices/sample/_09_ArmstrongNumber.java
@@ -0,0 +1,43 @@
+package dsa.practices.sample;
+
+import java.util.Scanner;
+
+public class _09_ArmstrongNumber {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number1");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter Number2");
+        int num2 = scanner.nextInt();
+        getArmstrongNumber(num1, num2);
+    }
+
+    private static void getArmstrongNumber(int num1, int num2) {
+        for(int i = num1; i <= num2; i++) {
+            findArmstrong(i);
+        }
+    }
+
+    private static void findArmstrong(int num){
+        int sum = 0;
+        int remainder;
+        int original = num;
+
+        int power = digitsOfNumber(num);
+        while (num > 0) {
+            remainder = num % 10;
+            sum = sum + ((int) Math.pow(remainder, power));
+            num = num / 10;
+        }
+        if(original == sum){
+            System.out.println(original + " is Armstrong Number");
+        }
+    }
+
+    private static int digitsOfNumber(int num){
+        String number = Integer.toString(num);
+        return number.length();
+    }
+}
