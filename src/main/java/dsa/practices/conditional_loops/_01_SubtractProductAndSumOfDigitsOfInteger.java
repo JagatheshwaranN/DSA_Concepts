74207561790d6ae0282a5d25175f0ca76214b9fc
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-28 17:00:28 +0530

    DSA - Loops Practice Programs

---
+++ src/main/java/dsa/practices/conditional_loops/_01_SubtractProductAndSumOfDigitsOfInteger.java
@@ -0,0 +1,23 @@
+package dsa.practices.conditional_loops;
+
+public class _01_SubtractProductAndSumOfDigitsOfInteger {
+
+    public static void main(String[] args) {
+
+        int number = 4421;
+        System.out.println(subtractProductAndSum(number));
+    }
+
+    private static int subtractProductAndSum(int num) {
+        int product = 1;
+        int sum = 0;
+        int remainder = 0;
+        while(num > 0){
+            remainder = num % 10;
+            product = product * remainder;
+            sum = sum + remainder;
+            num = num / 10;
+        }
+        return product-sum;
+    }
+}
---
+++ src/main/java/dsa/practices/conditional_loops/_04_Factorial_Of_Number.java
@@ -0,0 +1,20 @@
+package dsa.practices.conditional_loops;
+
+import java.util.Scanner;
+
+public class _04_Factorial_Of_Number {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println(findFactorial(input));
+    }
+
+    private static int findFactorial(int num){
+
+        int factorial = 0;
+
+        return factorial;
+    }
+}
