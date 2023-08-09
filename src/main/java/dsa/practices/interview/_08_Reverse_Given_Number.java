de33dfb38c2c773807c7d69b2350f045891ca423
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-09 14:40:40 +0530

    DSA - Practice Programs

---
+++ src/main/java/dsa/practices/interview/_08_Reverse_Given_Number.java
@@ -0,0 +1,23 @@
+package dsa.practices.interview;
+
+import java.util.Scanner;
+
+public class _08_Reverse_Given_Number {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to be reversed");
+        int number = scanner.nextInt();
+        findReverse(number);
+    }
+    private static void findReverse(int number) {
+        int num = number;
+        int reverse = 0;
+        while(number > 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+        System.out.println("The reverse of the number "+num+" is "+reverse);
+    }
+}
