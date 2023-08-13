e7ff3d52a9895909a10b6565df6b2f005a804b9c
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-13 13:43:12 +0530

    DSA - Leet Code Recursion Problem

--- src/main/java/dsa/leetcode_problems/_18_Number_Of_Steps_To_Reduce_Number_To_Zero_Recursion.java
+++ src/main/java/dsa/leetcode_problems/_18_Number_Of_Steps_To_Reduce_Number_To_Zero_Recursion.java
@@ -1,8 +1,28 @@
 package dsa.leetcode_problems;
 
+import java.util.Scanner;
+
 public class _18_Number_Of_Steps_To_Reduce_Number_To_Zero_Recursion {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to count the number of steps taken to make it Zero");
+        int number = scanner.nextInt();
+        System.out.println("The number of steps taken to make number " +number+" to zero is "+countSteps(number));
+    }
+    private static int countSteps(int number) {
+        return count(number, 0);
+    }
+    private static int count(int number, int step) {
+
+        if(number == 0)
+            return step;
+
+        if(number % 2 == 0){
+            return count(number / 2, step +1);
+        }else{
+            return count(number - 1, step +1);
+        }
     }
 }
