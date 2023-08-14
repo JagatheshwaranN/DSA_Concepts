d2b900387b5fd269366f910c3b5833dd95a5ce31
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-14 15:18:12 +0530

    DSA - Recursion Problems

---
+++ src/main/java/dsa/problems/_26_VerifyArraySortedOrNot_Recursion.java
@@ -0,0 +1,15 @@
+package dsa.problems;
+
+public class _26_VerifyArraySortedOrNot_Recursion {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 9};
+        System.out.println(checkArraySorted(arr, 0));
+    }
+    private static boolean checkArraySorted(int[] arr, int index) {
+
+        if(index == arr.length -1)
+            return true;
+        return arr[index] < arr[index + 1] && checkArraySorted(arr, index + 1);
+    }
+}
