284a494ae9cf192696c0ff3ad19e83f2de5a28f9
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-17 15:17:56 +0530

    DSA - Recursion Array Practice Problems

--- src/main/java/dsa/problems/_28_LinearSearch_All_Indexes_Recursion.java
+++ src/main/java/dsa/problems/_28_LinearSearch_All_Indexes_Recursion.java
@@ -10,6 +10,7 @@ public class _28_LinearSearch_All_Indexes_Recursion {
         int target = 3;
         ArrayList<Integer> result = findValueIndices(arr, target, 0, new ArrayList<>());
         System.out.println("The value "+target+" is found at indices "+result);
+        System.out.println("The value "+target+" is found at indices "+findValueIndicesWithList(arr, target, 0));
     }
     private static ArrayList<Integer> findValueIndices(int[] arr, int target, int index, ArrayList<Integer> list) {
 
@@ -18,5 +19,18 @@ public class _28_LinearSearch_All_Indexes_Recursion {
         if (arr[index] == target)
             list.add(index);
         return findValueIndices(arr, target, index + 1, list);
+    }
+
+    // Creation of ArrayList for every function call and add all the list before return.
+    // Not recommended to use. Shared for knowledge purpose.
+    private static ArrayList<Integer> findValueIndicesWithList(int[] arr, int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length)
+            return list;
+        if (arr[index] == target)
+            list.add(index);
+        ArrayList<Integer> resultFromSubsequentCalls =  findValueIndicesWithList(arr, target, index + 1);
+        list.addAll(resultFromSubsequentCalls);
+        return list;
     }
 }
