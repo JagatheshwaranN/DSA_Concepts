804252e18f9690497cb639a5c4097b6c98ec00e9
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-07-11 09:08:18 +0530

    Search Number Scenario Completed

---
+++ src/main/java/dsa/revise/SearchNumberAlgorithm3.java
@@ -0,0 +1,38 @@
+package dsa.revise;
+
+import java.util.Arrays;
+
+public class SearchNumberAlgorithm3 {
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10000000];
+        int searchNumber = 9999996;
+        for (int i = 1; i < numbers.length; i++) {
+            numbers[i] = i;
+        }
+        System.out.println(Arrays.toString(numbers));
+        long startTime = System.currentTimeMillis();
+        int number = findNumber(numbers, 0, numbers.length - 1, searchNumber);
+        System.out.printf("%d is found at %d attempt%n", number, count);
+        long endTime = System.currentTimeMillis();
+        System.out.printf("%d is found in %d ms%n", searchNumber, endTime - startTime);
+    }
+
+    static int findNumber(int[] numberCollection, int start, int end, int searchNumber) {
+        count++;
+        if (end >= start) {
+            int midPosition = start + (end - start) / 2;
+            if (numberCollection[midPosition] == searchNumber) {
+                return midPosition;
+            } else if (numberCollection[midPosition] > searchNumber) {
+                return findNumber(numberCollection, start, midPosition - 1, searchNumber);
+            } else {
+                return findNumber(numberCollection, midPosition + 1, end, searchNumber);
+            }
+        }
+        return -1;
+    }
+
+}
