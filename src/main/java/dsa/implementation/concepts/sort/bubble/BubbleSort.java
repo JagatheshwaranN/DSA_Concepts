6fc057910516963507e13e86c5df4b3831d4bf43
NJagatheshwaran <testjaga001@gmail.com>
2024-08-26 09:05:33 +0530

    DSA - Bubble Sort Implementation

---
+++ src/main/java/dsa/implementation/concepts/sort/bubble/BubbleSort.java
@@ -0,0 +1,24 @@
+package dsa.implementation.concepts.sort.bubble;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] input) {
+        for( int i = 0; i < input.length - 1; i ++) {
+            boolean swap = false;
+            for(int j = 0; j < input.length - i - 1; j++) {
+                if(input[j] > input[j+1]){
+                    swap = true;
+                    int temp = input[j];
+                    input[j] = input[j+1];
+                    input[j+1] = temp;
+                }
+            }
+            if(!swap){
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(input));
+    }
+}
