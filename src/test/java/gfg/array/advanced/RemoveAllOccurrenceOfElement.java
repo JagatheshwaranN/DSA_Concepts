5194f218c16fd49b90def0125d07f56469a580a1
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-09-17 21:45:21 +0530

    DSA - Remove Element Occurrence Completed.

--- src/test/java/gfg/array/advanced/RemoveAllOccurrenceOfElement.java
+++ src/test/java/gfg/array/advanced/RemoveAllOccurrenceOfElement.java
@@ -1,6 +1,10 @@
 package gfg.array.advanced;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RemoveAllOccurrenceOfElement {
 
@@ -8,7 +12,9 @@ public class RemoveAllOccurrenceOfElement {
         int[] numbers =  { 3, 9, 2, 3, 1, 7, 2, 3, 5 };
         int key = 3;
         removeElementOccurrenceUsingLoop(numbers, key);
-        removeElementOccurrenceUsingStream(numbers, key);
+        int[] numbers1 =  { 5, 9, 2, 5, 1, 7, 2, 5, 4 };
+        int key1 = 5;
+        removeElementOccurrenceUsingStream(numbers1, key1);
     }
 
     /**
@@ -34,13 +40,20 @@ public class RemoveAllOccurrenceOfElement {
         System.out.println(Arrays.toString(Arrays.copyOf(input, index)));
     }
 
+    /**
+     * The time complexity of the code is O(n). This is because the code iterates over
+     * each element of the input array exactly once, where n is the number of elements
+     * in the array.
+     * <p>
+     * The space complexity of the code is O(n). This is because, in the worst case,
+     * the filter operation creates an intermediate collection and the toArray() method
+     * creates a new array of size n, where n is the number of elements in the input array.
+     */
     private static void removeElementOccurrenceUsingStream(int[] input, int target) {
         int[] resultArray = Arrays.stream(input)
                 .filter(num -> num != target)
-                .boxed()
-                .mapToInt(Integer::intValue)
                 .toArray();
-        System.out.println(Arrays.toString(Arrays.copyOf(resultArray, resultArray.length)));
+        System.out.println(Arrays.toString(resultArray));
     }
 
 }
