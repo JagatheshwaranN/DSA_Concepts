aa7eb22bbfdd802abfbe334191e41cfdc72719c3
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-10-23 21:40:01 +0530

    DSA - LeetCode - Array Problems Added

---
+++ src/test/java/lc/array/SearchInsertPosition.java
@@ -0,0 +1,46 @@
+package lc.array;
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target
+ * is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ */
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,3,5,6};
+        int target = 5;
+        System.out.println(searchInsert(numbers, target));
+    }
+
+    private static int searchInsert(int[] nums, int target) {
+        if(nums.length == 0) {
+            return -1;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
+                return i;
+            }else {
+                if(nums[i] > target) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
--- src/test/java/lc/array/TwoSum.java
+++ src/test/java/lc/array/TwoSum.java
@@ -10,6 +10,20 @@ import java.util.Arrays;
  * use the same element twice.
  * <p>
  * You can return the answer in any order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ * <p>
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ * <p>
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
  */
 public class TwoSum {
 
